1ae6d0c7ec87bda83fe47f7f9170d44e9763ed86
Amit Genish <amitgk159753@gmail.com>
2025-07-04 17:15:02 +0300

    fix?

--- server.py
+++ server.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
-import ipaddress
 import logging
 
 from netfilterqueue import NetfilterQueue
 from netfilterqueue import Packet as NFQPacket
-from scapy.all import Raw, send
+from scapy.all import Raw, conf, get_if_addr, send
 from scapy.layers.inet import ICMP, IP
 
-# the client‐side public IP (where we send our replies)
-CLIENT_PUBLIC = "198.168.1.152"
-# the private subnet your client uses as inner‐src
-CLIENT_INNER_NET = ipaddress.ip_network("198.168.244.0/24")
+SERVER_IP = get_if_addr(conf.iface)
 
 ICMP_ID = 0x1234  # os.getpid() & 0xFFFF
 seq_reply = 0
@@ -20,13 +16,15 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 def server_cb(nf_pkt: NFQPacket):
     global seq_reply
+    client_ip = ""
     raw = nf_pkt.get_payload()
     ip = IP(raw)
 
     # 1) incoming echo-requests → unwrap & forward inner → accept
     if ip.proto == 1 and ip[ICMP].type == 8 and ip[ICMP].id == ICMP_ID:
+        client_ip = ip.src
         logging.debug("got icmp packet from tunnel")
-        inner = bytes(ip[ICMP].payload)
+        inner = ip[Raw].load
         # re‐inject into kernel so NAT + FORWARD apply:
         nf_pkt.set_payload(inner)
         nf_pkt.accept()
@@ -34,13 +32,12 @@ def server_cb(nf_pkt: NFQPacket):
 
     logging.debug(f"incoming ip packet {ip.summary()}")
     # 2) forwarded TCP replies to client‐inner → wrap & send back, drop
-    if ip.proto == 6 and ip.dst in CLIENT_INNER_NET:
-        logging.debug("got response from target")
-        inner = raw
+    if ip.proto == 6 and ip.dst == SERVER_IP:
+        logging.debug(f"got response from target, sending to {client_ip}")
         icmp = (
-            IP(dst=CLIENT_PUBLIC)
+            IP(dst=client_ip)
             / ICMP(type=0, code=0, id=ICMP_ID, seq=seq_reply)
-            / Raw(inner)
+            / Raw(raw)
         )
         send(icmp, verbose=False)
         seq_reply = (seq_reply + 1) & 0xFFFF
